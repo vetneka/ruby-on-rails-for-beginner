4537b79ae96ad6f17cdbf0810d786084d2d2f403
Andrei Akentev <akentev2010@gmail.com>
2024-04-18 08:14:01 +0500

    add comments destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,17 @@
 class CommentsController < ApplicationController
     def create
-        @article = Article.find(params[:article_id])
-        @comment = @article.comments.create(comment_params)
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.create(comment_params)
 
-        redirect_to article_path(@article)
+      redirect_to article_path(@article)
+    end
+
+    def destroy
+      @article = Article.find(params[:article_id])
+      @comment = @article.comments.find(params[:id])
+      @comment.destroy
+      
+      redirect_to article_path(@article), status: :see_other
     end
 
     private
